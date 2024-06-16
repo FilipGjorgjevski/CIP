fd5f0fc31f659143aedf5d0b2010d827a606ce30
FilipGjorgjevski <139113241+FilipGjorgjevski@users.noreply.github.com>
2024-06-16 16:32:00 +0200

    Create Class1-IO.java

---
+++ Class1-IO.java
@@ -0,0 +1,31 @@
+package CIP;
+
+import java.io.*;
+
+public class class1io {
+
+    public static void main(String[] args) throws IOException {
+        File f = new File("MyCode.txt");
+        f.createNewFile();
+        File dir = new File("NewChapter");
+        dir.mkdir();
+
+        if (dir.isDirectory())
+        {
+        String [] dirContents = dir.list(); 
+        for (int i =0; i < dirContents.length; ++i)
+         {
+        System.out.println(dirContents[i]);
+         }
+        }
+        System.out.println(dir.getAbsolutePath());
+        FileReader fileReader = new FileReader(f);
+        BufferedReader reader = new BufferedReader(fileReader);
+        String line = null;
+        while ((line = reader.readLine()) != null)
+        { 
+            System.out.println(line);
+        }
+
+    }
+}
